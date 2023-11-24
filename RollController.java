12c9241e62bcd21b4e1f7085fc7c996ea228efb3
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-11-24 12:57:21 -0500

    initial commit

---
+++ RollController.java
@@ -0,0 +1,39 @@
+package otel;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.sentry.Sentry;
+
+@RestController
+public class RollController {
+  private static final Logger logger = LoggerFactory.getLogger(RollController.class);
+
+  @GetMapping("/rolldice")
+  public String index(@RequestParam("player") Optional<String> player) {
+    int result = this.getRandomNumber(1, 6);
+    if (player.isPresent()) {
+      logger.info("{} is rolling the dice: {}", player.get(), result);
+    } else {
+      logger.info("Anonymous player is rolling the dice: {}", result);
+    }
+    return Integer.toString(result);
+  }
+  @GetMapping("/error")
+  public static void index(){
+    // this method throws a "NoSuchElementFoundException" exception
+    try {
+        throw new Exception("This is a test.");
+      } catch (Exception e) {
+        Sentry.captureException(e);
+      }
+  }
+
+  public int getRandomNumber(int min, int max) {
+    return ThreadLocalRandom.current().nextInt(min, max + 1);
+  }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+  id("java")
+  id("org.springframework.boot") version "3.0.6"
+  id("io.spring.dependency-management") version "1.1.0"
+}
+
+sourceSets {
+  main {
+    java.setSrcDirs(setOf("."))
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation ("io.sentry:sentry-spring-boot-starter-jakarta:6.34.0")
+  implementation ("io.sentry:sentry-opentelemetry-core:6.34.0")
+}
